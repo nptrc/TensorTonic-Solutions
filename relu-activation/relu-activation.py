56397b076bab169510f700eede530dca5af55840
Null_Pointer_Crash <86598978+nptrc@users.noreply.github.com>
2026-09-02 06:12:56 +0700

    Solve relu-activation

---
+++ relu-activation/relu-activation.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def relu(x) -> np.ndarray:
+    """
+    Returns a NumPy array with the same shape as x.
+    """
+    arr = np.asarray(x, dtype=np.float32)
+    return np.array(np.maximum(0.0, x))
